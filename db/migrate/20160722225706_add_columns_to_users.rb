542f243d851fdd17b06e83ad696a8a784efff34c
Christel Trutmann & Ed Mechem & Marshall Agharanya  & Michael Furlong <pair+ctrutmann.edmechem.marshyup.mfurlong64@devbootcamp.com>
2016-07-22 16:19:42 -0700

    add follows model

---
+++ db/migrate/20160722225706_add_columns_to_users.rb
@@ -0,0 +1,38 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.0]
+  def change
+    # bio
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+    add_column :users, :zipcode, :integer
+    add_column :users, :country, :string
+    add_column :users, :cohort_id, :integer
+    add_column :users, :profile_image, :image
+    add_column :users, :love, :string
+    add_column :users, :quirk, :string
+    add_column :users, :bio, :string
+    add_column :users, :is_admin, :string, default: false
+    add_column :users, :employment_status, :string
+    add_column :users, :employer, :string
+    add_column :users, :role, :string
+    add_column :users, :advice_to_students, :string
+    add_column :users, :advice_to_graduates, :string
+
+    # social networks
+    add_column :users, :linkedin, :string
+    add_column :users, :github, :string
+    add_column :users, :facebook, :string
+    add_column :users, :twitter, :string
+    add_column :users, :employ, :string
+    add_column :users, :personal_website, :string
+
+    # data information
+    add_column :users, :female_scholarship, :string
+    add_column :users, :poc_scholarship, :string
+    add_column :users, :lgbtq_scholarship, :string
+    add_column :users, :veteran_scholarship, :string
+    add_column :users, :prior_coding_experience, :string
+
+  end
+end
