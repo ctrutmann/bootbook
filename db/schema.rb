72a5422eb12d28b55efcdf053752a1278ec8f5d4
Christel Trutmann & Ed Mechem & Marshall Agharanya  & Michael Furlong <pair+ctrutmann.edmechem.marshyup.mfurlong64@devbootcamp.com>
2016-07-22 16:30:18 -0700

    add model for posts and interests

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722231814) do
+ActiveRecord::Schema.define(version: 20160722232855) do
 
   create_table "follows", force: :cascade do |t|
     t.integer  "followee_id"
     t.integer  "follower_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "interests", force: :cascade do |t|
+    t.string   "interest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.string   "post_type_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "users", force: :cascade do |t|
