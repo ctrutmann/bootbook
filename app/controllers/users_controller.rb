213ab7dbce292d2c0467387c2a05eee353727e5a
Ed Mechem & Michael Furlong <pair+edmechem.mfurlong64@devbootcamp.com>
2016-07-22 20:33:21 -0700

    being user controller framework with basic routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,69 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    # Add the current user's owned post
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def delete
+    # You may never leave BootBook MUHAHAHAHA
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :id,
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :city,
+      :state,
+      :zipcode,
+      :country,
+      :love,
+      :quirk,
+      :is_admin,
+      :is_graduate,
+      :employment_status,
+      :role,
+      :bio,
+      :cohort_id,
+      :profile_image,
+      :female_scholarship,
+      :poc_scholarship,
+      :lgbtq_scholarship,
+      :veteran_scholarship,
+      :linkedin,
+      :github,
+      :facebook,
+      :twitter,
+      :employ,
+      :prior_coding_experience,
+      :personal_website,
+      :employer,
+      :advice_to_students,
+      :advice_to_graduates
+      )
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get 'users/index'
-
   get 'welcome/index'
+  # Don't forget about resource: :users
+
+  resources :users
+  resources :posts
+
+  root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
