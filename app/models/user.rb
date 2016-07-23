42f2b6e3ddf861a0873cff9a03d9cef9a70f4de8
Marshall Agharanya <marshall.agharanya@gmail.com>
2016-07-23 00:12:53 -0700

    add omniauth config in devise initializer file with client id and app secret

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,5 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
     end
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-
+  config.omniauth :github, '60b5ce02b5996125c58a', 'd8426a91ac48d54de307fe5c38f3a4d364fa4fe3', :scope => 'user:email'
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
