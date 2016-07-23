e09c711b601eba1d0b9551abcac3e9d1fefb4be7
Christel Trutmann & Marshall Agharanya <pair+ctrutmann.marshyup@devbootcamp.com>
2016-07-23 15:15:28 -0700

    Add validations to Conversation model

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,5 +2,13 @@ class Conversation < ApplicationRecord
   belongs_to :sender, class_name: 'User', foreign_key: :sender_id
   belongs_to :recipient, class_name: 'User', foreign_key: :recipient_id
   belongs_to :variety, class_name: 'ConversationVariety', foreign_key: :conversation_variety_id
-  has_many :messages
+  has_many :messages, dependent: :destroy
+
+  validates_presence_of :sender_id, :recipient_id, :conversation_variety_id
+  validates_uniqueness_of :sender_id, scope: :recipient_id
+
+  scope :between, -> (sender_id,recipient_id) do
+    where('(conversations.sender_id = ? AND conversations.recipient_id =?) OR (conversations.sender_id = ? AND conversations.recipient_id =?)', sender_id,recipient_id, recipient_id, sender_id)
+  end
+
 end
