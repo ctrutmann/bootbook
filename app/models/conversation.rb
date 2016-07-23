5ea50c7f4be0c4c199eddae650d9f1fd929f7add
Christel Trutmann & Marshall Agharanya <pair+ctrutmann.marshyup@devbootcamp.com>
2016-07-23 13:03:50 -0700

    Refine and test User/Conversation/ConversationVariety associations--all functioning

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,5 +1,5 @@
 class Conversation < ApplicationRecord
   belongs_to :sender, class_name: 'User', foreign_key: :sender_id
   belongs_to :recipient, class_name: 'User', foreign_key: :recipient_id
-  belongs_to :conversation_variety
+  belongs_to :variety, class_name: 'ConversationVariety', foreign_key: :conversation_variety_id
 end
