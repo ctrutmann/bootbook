6361b469bbb405aeae3d4370ff3ad8f40ded41e0
Ed Mechem & Marshall Agharanya <pair+edmechem.marshyup@devbootcamp.com>
2016-07-23 19:49:16 -0700

    add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   resources :users
 
   root 'welcome#index'
+  get   '/login', :to => 'sessions#new', :as => :login
+  match '/auth/:provider/callback', :to => 'sessions#create'
+  match '/auth/failure', :to => 'sessions#failure'
+
 end
