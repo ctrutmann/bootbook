f87634ff0c6a4e25a71efdfddec8a2a9cb3a1f62
Ed Mechem & Marshall Agharanya <pair+edmechem.marshyup@devbootcamp.com>
2016-07-24 13:28:41 -0700

    Remove commented-out debugging attempt from omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :github, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET'], scope: 'user:email'
 
-  # provider :github, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret
 end
