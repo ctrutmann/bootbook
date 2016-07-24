c95eb6ba729d82ab483afffcc389c85004930f04
Ed Mechem & Marshall Agharanya <pair+edmechem.marshyup@devbootcamp.com>
2016-07-23 20:44:47 -0700

    Generate sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/failure'
+
   resources :users
 
   root 'welcome#index'
   get   '/login', :to => 'sessions#new', :as => :login
-  match '/auth/:provider/callback', :to => 'sessions#create'
-  match '/auth/failure', :to => 'sessions#failure'
+  match '/auth/:provider/callback', :via => [:get], :to => 'sessions#create'
+  match '/auth/failure', :via => [:get], :to => 'sessions#failure'
 
 end
