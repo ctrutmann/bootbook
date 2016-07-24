5d8d4ddb3cf8bc1b5e5cffaa7af7fa7ac9d99751
Christel Trutmann & Ed Mechem & Marshall Agharanya  & Michael Furlong <pair+ctrutmann.edmechem.marshyup.mfurlong64@devbootcamp.com>
2016-07-23 18:23:42 -0700

    Remove all reference to devise and comment out omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,21 +11,12 @@ class User < ApplicationRecord
   has_many :followees, through: :followee_follows, source: :followee
   has_many :followee_follows, foreign_key: :follower_id, class_name: "Follow"
 
-
-
-
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable#, :confirmable
-
-  def self.from_omniauth(auth)
-    where(provider:auth.provider, uid:auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-    end
-  end
+  # def self.from_omniauth(auth)
+  #   where(provider:auth.provider, uid:auth.uid).first_or_create do |user|
+  #     user.provider = auth.provider
+  #     user.uid = auth.uid
+  #     user.email = auth.info.email
+  #     user.password = Devise.friendly_token[0,20]
+  #   end
+  # end
 end
