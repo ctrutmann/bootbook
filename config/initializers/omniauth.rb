b5186203b88ab00804be750f32d1123b00079503
Ed Mechem & Marshall Agharanya <pair+edmechem.marshyup@devbootcamp.com>
2016-07-24 13:25:20 -0700

    Remove debugging artifacts from omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,6 @@
-Dotenv.load
-
 Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :github, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET'], scope: 'user:email'
-#
-  # provider :github, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret
 
+  # provider :github, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret
 end
