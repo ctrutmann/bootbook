1ceb32c78d6ef5f4ab7a565eb9e40391e8ee3e76
Christel Trutmann & Ed Mechem <pair+ctrutmann.edmechem@devbootcamp.com>
2016-07-25 10:33:16 -0700

    Change sessions redirect for first-time user, working on user edit form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,20 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    @user = User.where(:provider => auth['provider'],
-                      :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
-    reset_session
-    session[:user_id] = @user.id
-    flash[:success] = "Quirk!" if !current_user.quirk
-    redirect_to user_path(@user)
+    # @user = User.where(:provider => auth['provider'], :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
+
+    @user = User.where(:provider => auth['provider'], :uid => auth['uid'].to_s).first
+    if @user
+      reset_session
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      @user = User.create_with_omniauth(auth)
+      reset_session
+      session[:user_id] = @user.id
+      flash[:success] = "Quirk!" if !current_user.quirk
+      redirect_to edit_user_path(@user) #do something differently here
+    end
   end
 
   def destroy
