d7542a8dd712179457bdf29e4f47b7218fa40465
Christel Trutmann & Ed Mechem & Marshall Agharanya  & Michael Furlong <pair+ctrutmann.edmechem.marshyup.mfurlong64@devbootcamp.com>
2016-07-24 19:00:23 -0700

    Add authentication requirements to all but login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new, :create]
 
   def new
     redirect_to '/auth/github'
@@ -10,14 +11,14 @@ class SessionsController < ApplicationController
                       :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
     reset_session
     session[:user_id] = @user.id
-    flash[:success] = "Quirk!"
+    flash[:success] = "Quirk!" if !current_user.quirk
     redirect_to user_path(@user)
   end
 
-  # def destroy
-  #   reset_session
-  #   redirect_to root_url, :notice => 'Signed out!'
-  # end
+  def destroy
+    reset_session
+    redirect_to root_url
+  end
 
   def failure
     redirect_to root_url, :alert => "Authentication error: #{params[:message].humanize}"
