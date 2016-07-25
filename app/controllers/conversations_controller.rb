f2bba58a238304aa20f8e0492be23397de25f7a6
Ed Mechem & Michael Furlong <pair+edmechem.mfurlong64@devbootcamp.com>
2016-07-24 20:23:43 -0700

    limit access to direct message conversations

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,13 +1,15 @@
 class ConversationsController < ApplicationController
   def index
-    @conversations = Conversation.where(sender_id: current_user.id)
-    @conversations += Conversation.where(recipient_id: current_user.id)
+      @conversations = Conversation.where(sender_id: current_user.id)
+      @conversations += Conversation.where(recipient_id: current_user.id)
   end
-
-
 
   def show
     @conversation = Conversation.find(params[:id])
+    if current_user.id == @conversation.sender_id || current_user.id ==  @conversation.recipient_id
+    else
+      redirect_to '/'
+    end
   end
 
   def edit
