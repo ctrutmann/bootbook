6322a6874d498aa1b56412936c460de422fcf9e5
Christel Trutmann <ctrutmann@gmail.com>
2016-07-26 22:32:58 -0700

    Remove commented out detritus from config/routes.rb, comment auth/failure back in

--- config/routes.rb
+++ config/routes.rb
@@ -5,24 +5,16 @@ Rails.application.routes.draw do
     get 'search', :on => :collection
   end
 
-  # # works
-  # get '/users/search' =>'users#search', :as => :search
-
-  # resources :conversations do
-  #   resources :messages
-  # end
-
   resources :conversations do
     resources :messages
   end
 
-  # match '/auth/:provider/callback', :via => [:get], :to => 'sessions#create'
   match '/auth/failure', :via => [:get], :to => 'sessions#failure'
 
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signin' => 'sessions#new', :as => :signin
   get '/signout' => 'sessions#destroy', :as => :signout
-  # get '/auth/failure' => 'sessions#failure'
+  get '/auth/failure' => 'sessions#failure'
 
   root 'welcome#index'
 end
