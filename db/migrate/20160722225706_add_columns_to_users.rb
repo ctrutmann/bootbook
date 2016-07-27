5ef40aa58f0536d8ad981312bb82fb59f3d1b364
Christel Trutmann & Michael Furlong <pair+ctrutmann.mfurlong64@devbootcamp.com>
2016-07-27 12:39:53 -0700

    Rearranged users migration fields for clarity

--- db/migrate/20160722225706_add_columns_to_users.rb
+++ db/migrate/20160722225706_add_columns_to_users.rb
@@ -1,36 +1,28 @@
 class AddColumnsToUsers < ActiveRecord::Migration[5.0]
   def change
-    # Required fields
+      # Oauth fields
+    add_column :users, :uid, :integer
+    add_column :users, :provider, :string
+
+    # Populated by Github OAuth
     add_column :users, :name, :string
     add_column :users, :email, :string
+    add_column :users, :profile_image, :string
+
+    # Required Personal Info
     add_column :users, :city, :string
     add_column :users, :state, :string
     add_column :users, :postal_code, :string
     add_column :users, :country, :string
-    add_column :users, :profile_image, :string
-    add_column :users, :graduation_date, :date
-    add_column :users, :is_graduate, :boolean, default: false
-    add_column :users, :is_admin, :boolean, default: false
 
-    # Oauth fields
-    add_column :users, :uid, :integer
-    add_column :users, :provider, :string
+    # Fields Not Editable by User
+    add_column :users, :graduation_date, :date
+    add_column :users, :is_admin, :boolean, default: false
 
     # DBC fields
     add_column :users, :love, :string
     add_column :users, :quirk, :string
-    add_column :users, :bio, :text
-    add_column :users, :advice_to_students, :text
-    add_column :users, :advice_to_graduates, :text
-
-    # Notification preferences
-    add_column :users, :willing_to_mentor,:boolean, default: true
-    add_column :users, :willing_to_collaborate,:boolean, default: true
-
-    # Employment fields
-    add_column :users, :employment_status, :string, default: "unemployed"
-    add_column :users, :employer, :string
-    add_column :users, :role, :string
+    add_column :users, :is_graduate, :boolean, default: false
 
     # Social links fields
     add_column :users, :linkedin, :string
@@ -40,6 +32,15 @@ class AddColumnsToUsers < ActiveRecord::Migration[5.0]
     add_column :users, :employ, :string
     add_column :users, :personal_website, :string
 
+    # Employment fields
+    add_column :users, :employment_status, :string, default: "unemployed"
+    add_column :users, :employer, :string
+    add_column :users, :role, :string
+
+    # Notification preferences
+    add_column :users, :willing_to_mentor,:boolean, default: true
+    add_column :users, :willing_to_collaborate,:boolean, default: true
+
     # Demographic fields
     add_column :users, :female_scholarship, :boolean, default: false
     add_column :users, :poc_scholarship, :boolean, default: false
@@ -47,5 +48,9 @@ class AddColumnsToUsers < ActiveRecord::Migration[5.0]
     add_column :users, :veteran_scholarship, :boolean, default: false
     add_column :users, :prior_coding_experience, :string
 
+    # Text Fields
+    add_column :users, :bio, :text
+    add_column :users, :advice_to_students, :text
+    add_column :users, :advice_to_graduates, :text
   end
 end
