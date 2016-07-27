95ee3aa15b2377b5999c72d3eb76414bb5911e77
Marshall Agharanya <marshall.agharanya@gmail.com>
2016-07-26 23:29:40 -0700

    add heroku keys to env file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  provider :github, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET'], scope: 'user:email'
-
+  # provider :github, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET'], scope: 'user:email'
+    provider :heroku, ENV.fetch('HEROKU_OAUTH_ID'), ENV.fetch('HEROKU_OAUTH_SECRET'), scope: 'user:email'
 end
