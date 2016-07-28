5bbcd15c82c420d2df8a6f9dcc3e0f6d1cce8537
Ed Mechem <email": null>
2016-07-28 11:30:45 -0700

    Restore Grid view as default; center map marker pic & info; streamline JS functions

--- app/assets/javascripts/bootbook.js
+++ app/assets/javascripts/bootbook.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
   toggleView();
-  initMap();
-  usersIndex();
+  // initMap();
+  // usersIndex();
 })
 
 function toggleView() {
   $("#toggle-view").on("click", function(event) {
     event.preventDefault();
     if ($('#map-view').is(':hidden')) {
+      initMap();
+      populateMapViewWithAllBoots();
       $('#grid-view').hide();
       $('#map-view').show();
       $('#toggle-view')[0].text = "Show as Grid"
@@ -28,28 +30,31 @@ function initMap() {
       fullscreenControl: false,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
-
     var defaultBounds = new google.maps.LatLngBounds(
       new google.maps.LatLng(37.784580, -122.397437),
       new google.maps.LatLng(37.784580, -122.397437)
     );
     var options = { bounds: defaultBounds };
-    }
-};
+  }
+}
 
-function usersIndex() {
-  $(".navbar-brand").on("click", function(event) {
-    event.preventDefault();
-    $.ajax({
-      url: '/users',
-      method: 'GET',
-      dataType: 'json'
-    }).done(function(response){
-      initMap();   // maybe don't need this here again...
-      formatDataAndPlaceMarker(response.filecontent);
-    }).fail(function(response){
-      console.log("shit");
-    });
+// function usersIndex() {
+//   $(".navbar-brand").on("click", function(event) {
+//     event.preventDefault();
+//     populateMapViewWithAllBoots();
+//   });
+// }
+
+function populateMapViewWithAllBoots() {
+  $.ajax({
+    url: '/users',
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(response){
+    initMap();   // maybe don't need this here again...
+    formatDataAndPlaceMarker(response.filecontent);
+  }).fail(function(response){
+    console.log("shit");
   });
 }
 
@@ -95,7 +100,7 @@ function generateLocationStringShort(user) {
     };
   };
   if (country != null) {
-    if (country != "US") {
+    if (country != "United States") {
       if (locationString != "") {
         locationString += (' ' + country);
       } else {
@@ -128,7 +133,7 @@ function generateLocationStringLong(user) {
     }
   };
   if (country != null) {
-    if (country != "US") {
+    if (country != "United States") {
       if (locationString != "") {
         locationString += (' ' + country);
       } else {
@@ -141,8 +146,8 @@ function generateLocationStringLong(user) {
 
 function constructInfoWindowContent(profile_image, name, userPageLink, locationString) {
   contentString =
-    '<div id="content">' +
-      '<div><a href="' + userPageLink + '"><img src="' + profile_image + '" width="40" height="40" class="img-responsive marker-image"></a></div><span class="marker-name"><a href="' + userPageLink + '">' + name + '</a></span>' +
+    '<div class="info-window-content">' +
+      '<div><a href="' + userPageLink + '"><img src="' + profile_image + '" width="40" height="40" class="center marker-image" alt="user picture"></a></div><div class="marker-name"><a href="' + userPageLink + '">' + name + '</a></div>' +
       '<div id="bodyContent">' +
         '<p>' + locationString + '<p>' +
       '</div>' +
@@ -157,8 +162,11 @@ function setMarker(latLng, infoWindowContent) {
     position: latLng,
   });
   infowindow.setContent(infoWindowContent);
-  // infowindow.open(map, marker);
   marker.addListener('click', function() {
-    infowindow.open(map, marker)
+    if (infowindow.anchor == null) {
+      infowindow.open(map, marker);
+    } else {
+      infowindow.close();
+    }
   })
 }
