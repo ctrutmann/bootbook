8514338ee892b735c3667500ed3c374b8eac0f35
Marshall Agharanya <marshall.agharanya@gmail.com>
2016-07-28 11:27:58 -0700

    adjust logic in create message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,13 +25,13 @@ class MessagesController < ApplicationController
     @conversation = Conversation.find(params[:conversation_id])
     @message = @conversation.messages.new(message_params)
     @messages = @conversation.messages
+    if @conversation.messages.count == 0 && @message.content != nil
+       UserMailer.new_convo(@user).deliver
+    end
     if @message.save && @message.content != nil
       @conversation.touch(:updated_at)
       @conversation.save
       redirect_to conversation_messages_path(@conversation)
-      if @conversation.messages.count == 0
-        UserMailer.new_convo(@user).deliver
-      end
     else
       redirect_to conversation_messages_path
     end
